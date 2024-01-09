2f400de156299d98d6443b031ee94d9657a4a306
matt <mwilkerson1@broncos.uncfsu.edu>
2024-01-09 10:37:26 -0500

    restructured directory, implemented cross-val, added scripts for img aug

--- Img-Classifier_ResNet50-MSL-v2_1/1__Code_ResNet50-MSL-v2_1/NUC_load_benchmark.py
+++ Img-Classifier_ResNet50-MSL-v2_1/1__Code_ResNet50-MSL-v2_1/NUC_load_benchmark.py
@@ -4,15 +4,9 @@ from PIL import Image
 import os
 import psutil
 import matplotlib.pyplot as plt
-import pynvml
 import time
-from fvcore.nn import FlopCountAnalysis
 
 MDL_PATH = '/home/mwilkers1/Documents/Projects/IMPACT/Edge-AI/IMPACT-Edge_AI/Img-Classifier_ResNet50-MSL-v2_1/2__TorchScript_Saved_Model/99percent-ResNet50-v2.1/mars_classifier_scripted.pt'
-
-# Initialize NVML for GPU metrics
-pynvml.nvmlInit()
-handle = pynvml.nvmlDeviceGetHandleByIndex(0)  
 
 # Load the TorchScript model
 model = torch.jit.load(MDL_PATH)
@@ -30,16 +24,14 @@ image_files = os.listdir(image_directory)
 
 # Metrics storage
 cpu_usage = []
-gpu_utilization = []
-gpu_memory = []
 io_time = []
 prepoc_time = []
 ram_usage = []
 
 # Check if a GPU is available and if so, use it
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-#device = torch.device('cpu')
 model.to(device)
+print(f"Module loaded to {device}...")
 total_time = 0
 
 # Loop through images in the directory and make predictions
@@ -66,10 +58,6 @@ for image_name in image_files:
         # Record CPU and GPU metrics before prediction
         cpu_percent = psutil.cpu_percent()
         cpu_usage.append(cpu_percent)
-        gpu_util = pynvml.nvmlDeviceGetUtilizationRates(handle)
-        gpu_utilization.append(gpu_util.gpu)
-        gpu_mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-        gpu_memory.append(gpu_mem_info.used)
         ram_usage.append(psutil.virtual_memory().percent)
 
         # Predict the image class
@@ -81,6 +69,7 @@ for image_name in image_files:
             
         # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
         probabilities = torch.nn.functional.softmax(output[0], dim=0)
+        print(f"Image: {image_name}")
 
     except Exception as e:
         print(f"An error occurred while processing the image '{image_name}': {e}")
@@ -88,8 +77,6 @@ for image_name in image_files:
 # Print the average inference time, CPU usage, GPU utilization, GPU memory usage, IO Time, Preprocessing Time, and RAM usage
 print(f"Average inference time: {total_time / len(image_files) * 1000} milliseconds")
 print(f"Average CPU usage: {sum(cpu_usage) / len(cpu_usage)}%")
-print(f"Average GPU utilization: {sum(gpu_utilization) / len(gpu_utilization)}%")
-print(f"Average GPU memory usage: {sum(gpu_memory) / len(gpu_memory) / (1024**2)} MB")
 print(f"Average IO Time: {sum(io_time) / len(io_time) * 1000} milliseconds")
 print(f"Average Preprocessing Time: {sum(prepoc_time) / len(prepoc_time) * 1000} milliseconds")
 print(f"Average RAM usage: {sum(ram_usage) / len(ram_usage)}%")
@@ -107,22 +94,6 @@ plt.title('CPU Usage')
 plt.ylim(0, 100)  # Set the y-axis limit to 100
 plt.legend()
 
-plt.subplot(1, 4, 2)
-plt.plot(gpu_utilization, label='GPU Utilization (%)')
-plt.xlabel('Image Index')
-plt.ylabel('Percentage')
-plt.title('GPU Utilization')
-plt.ylim(0, 100)  # Set the y-axis limit to 100
-plt.legend()
-
-plt.subplot(1, 4, 3)
-plt.plot([mem / (1024**2) for mem in gpu_memory], label='GPU Memory (MB)')
-plt.xlabel('Image Index')
-plt.ylabel('Memory Usage (MB)')
-plt.title('GPU Memory Usage')
-plt.ylim(0, 12288)  # Set the y-axis limit to 12288
-plt.legend()
-
 plt.subplot(1, 4, 4)
 plt.plot(ram_usage, label='RAM Usage (%)')
 plt.xlabel('Image Index')
@@ -133,6 +104,3 @@ plt.legend()
 
 plt.tight_layout()
 plt.show()
-
-# Clean up NVML
-pynvml.nvmlShutdown()
